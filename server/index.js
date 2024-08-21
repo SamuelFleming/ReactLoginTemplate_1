93cc2f55eea77cc0a605f0084be571a8e0f67983
SamuelFLeming <flemo1485@gmail.com>
2024-08-21 15:09:16 +1000

    Simple password check now working

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const users = require('./users');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors())
 app.use(express.json());
 
 app.post('/login', (req, res) => {
