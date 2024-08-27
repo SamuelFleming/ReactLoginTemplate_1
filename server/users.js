96596cfb0408dcebc80e0888b388345e3da36fcc
SamuelFLeming <flemo1485@gmail.com>
2024-08-27 13:45:14 +1000

    adding more users

--- server/users.js
+++ server/users.js
@@ -2,7 +2,9 @@ const users = [
     { username: 'admin', password: 'adminpass' },
     { username: 'user1', password: 'pass1' },
     { username: 'user2', password: 'pass2' },
-    { username: 'user3', passwrok: 'pass3' }
+    { username: 'user3', password: 'pass3' },
+    { username: 'user4', password: 'pass4' },
+    { username: 'user5', password: 'pass5' }
 ];
 
 module.exports = users;
